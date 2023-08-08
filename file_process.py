61fb2b57b82a1bd7c65daffff56d607ed88ff80f
ZhiJie88 <111533492+ZhiJie88@users.noreply.github.com>
2023-08-07 20:03:10 -0500

    Update file_process.py
    one graph

--- file_process.py
+++ file_process.py
@@ -7,7 +7,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 selected_files = []  # Store the selected file paths
-selected_x_columns = []  # Store the selected x-columns
+selected_x_column = ""  # Store the selected x-column
 selected_y_columns = []  # Store the selected y-columns
 
 def browse_files():
@@ -32,10 +32,10 @@ def update_column_selector():
         except Exception as e:
             messagebox.showerror("Error", str(e))
 
-def set_x_columns(event):
-    global selected_x_columns
+def set_x_column(event):
+    global selected_x_column
     widget = event.widget
-    selected_x_columns = [widget.get(idx).strip() for idx in widget.curselection()]
+    selected_x_column = widget.get(widget.curselection()).strip()
 
 def set_y_columns(event):
     global selected_y_columns
@@ -44,7 +44,7 @@ def set_y_columns(event):
 
 def plot_data():
     try:
-        if len(selected_x_columns) < 1 or len(selected_x_columns) > 1:
+        if not selected_x_column:
             messagebox.showwarning("Invalid Selection", "Please select an x-column.")
             return
         if len(selected_y_columns) < 1:
@@ -60,26 +60,25 @@ def plot_data():
             try:
                 df = pd.read_csv(file_path.strip())
                 
-                # Remove rows with missing values
-                df.dropna(subset=selected_x_columns + selected_y_columns, inplace=True)
+                df.dropna(subset=[selected_x_column] + selected_y_columns, inplace=True)  # Remove rows with missing values in selected columns
 
                 if x_min is None:
-                    x_min = df[selected_x_columns].min().min()
-                    x_max = df[selected_x_columns].max().max()
+                    x_min = df[selected_x_column].min()
+                    x_max = df[selected_x_column].max()
                 else:
-                    x_min = min(x_min, df[selected_x_columns].min().min())
-                    x_max = max(x_max, df[selected_x_columns].max().max())
+                    x_min = min(x_min, df[selected_x_column].min())
+                    x_max = max(x_max, df[selected_x_column].max())
 
                 for y_col in selected_y_columns:
-                    plt.plot(df[selected_x_columns[0]], df[y_col], 'o', markersize=2, label=os.path.basename(file_path))
+                    plt.plot(df[selected_x_column], df[y_col], 'o', markersize=2, label=f"{y_col} - {os.path.basename(file_path)}")
 
             except pd.errors.ParserError as pe:
                 messagebox.showwarning("Invalid Data", f"Skipped {os.path.basename(file_path)}: {str(pe)}")
                 continue
 
-        plt.xlabel(selected_x_columns[0])
+        plt.xlabel(selected_x_column)
         plt.ylabel(', '.join(selected_y_columns))
-        plt.title('Data from CSV Files')
+        plt.title(f'Data from CSV Files: {selected_x_column} vs {", ".join(selected_y_columns)}')
         plt.legend()
 
         plt.tight_layout()  # Adjust the spacing of the plot
@@ -118,13 +117,13 @@ btn_browse = tk.Button(window, text="Browse", command=browse_files)
 btn_browse.grid(row=2, column=1, padx=5, pady=5)
 
 # Create the "X Column Selection" label
-label_x_columns = tk.Label(window, text="X Columns:")
+label_x_columns = tk.Label(window, text="X Column:")
 label_x_columns.grid(row=3, column=0, padx=5, pady=5, sticky="w")
 
 # Create the "X Columns" listbox
-listbox_x_columns = tk.Listbox(window, selectmode=tk.MULTIPLE, exportselection=0)
+listbox_x_columns = tk.Listbox(window, selectmode=tk.SINGLE, exportselection=0)
 listbox_x_columns.grid(row=4, column=0, padx=5, pady=5)
-listbox_x_columns.bind("<<ListboxSelect>>", set_x_columns)
+listbox_x_columns.bind("<<ListboxSelect>>", set_x_column)
 
 # Create the "Y Column Selection" label
 label_y_columns = tk.Label(window, text="Y Columns:")
