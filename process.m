a8c16f4d7d98dd385647205a60c8046cb7d7c025
ZhiJie88 <111533492+ZhiJie88@users.noreply.github.com>
2023-08-20 11:15:23 -0500

    Update process.m

--- process.m
+++ process.m
@@ -1,75 +1,70 @@
-% Clear workspace and close all figures
-clear;
-close all;
+function CSVDataPlotter
+    % Create the main GUI figure
+    mainFigure = figure('Name', 'CSV Data Plotter', 'Position', [100, 100, 400, 300]);
 
-% Create the main GUI figure
-mainFigure = figure('Name', 'CSV Data Plotter', 'Position', [100, 100, 400, 300]);
+    % Create listbox for file selection
+    fileListbox = uicontrol('Style', 'listbox', 'Position', [20, 150, 150, 120], 'String', {});
 
-% Initialize list of filenames
-fileNames = {};
+    % Create button to load files
+    loadButton = uicontrol('Style', 'pushbutton', 'Position', [20, 120, 100, 20], 'String', 'Load Files', 'Callback', @loadFiles);
 
-% Create listbox for file selection
-fileListbox = uicontrol('Style', 'listbox', 'Position', [20, 150, 150, 120], 'String', fileNames);
+    % Create axis for plotting
+    plotAxis = axes('Parent', mainFigure, 'Position', [0.4, 0.1, 0.55, 0.7]);
 
-% Create button to load files
-loadButton = uicontrol('Style', 'pushbutton', 'Position', [20, 120, 100, 20], 'String', 'Load Files', 'Callback', @loadFiles);
+    % Callback function for the Load Files button
+    function loadFiles(~, ~)
+        % Get a list of CSV files in the current directory
+        csvFiles = dir('*.csv');
 
-% Create axis for plotting
-plotAxis = axes('Parent', mainFigure, 'Position', [0.4, 0.1, 0.55, 0.7]);
+        % Extract filenames
+        fileNames = {csvFiles.name};
 
-% Callback function for the Load Files button
-function loadFiles(~, ~)
-    % Get a list of CSV files in the current directory
-    csvFiles = dir('*.csv');
-    
-    % Extract filenames
-    fileNames = {csvFiles.name};
-    
-    % Update listbox with file names
-    set(fileListbox, 'String', fileNames, 'Value', []);
-end
-
-% Callback function for listbox selection
-set(fileListbox, 'Callback', @selectFile);
-
-function selectFile(~, ~)
-    selectedIndices = get(fileListbox, 'Value');
-    
-    % Clear previous data
-    data = cell(0, 2);
-    
-    % Read data from selected CSV files and store in 'data' variable
-    for i = selectedIndices
-        filename = fileNames{i};
-        fileData = csvread(filename);
-        data{end+1, 1} = filename;
-        data{end, 2} = fileData;
+        % Update listbox with file names
+        set(fileListbox, 'String', fileNames, 'Value', []);
     end
-    
-    % Update the plot with the selected data
-    plotSelectedData(data);
-end
 
-% Callback function to plot selected data
-function plotSelectedData(selectedData)
-    cla(plotAxis); % Clear the plot
-    
-    hold(plotAxis, 'on');
-    for i = 1:length(selectedData)
-        filename = selectedData{i, 1};
-        fileData = selectedData{i, 2};
-        
-        xData = fileData(:, 1);
-        yData = fileData(:, 2:end);
-        
-        for j = 1:size(yData, 2)
-            plot(plotAxis, xData, yData(:, j), 'DisplayName', [filename, ' - Y', num2str(j)]);
+    % Callback function for listbox selection
+    set(fileListbox, 'Callback', @selectFile);
+
+    function selectFile(source, ~)
+        selectedIndices = get(source, 'Value');
+
+        % Clear previous data
+        data = cell(0, 2);
+
+        % Read data from selected CSV files and store in 'data' variable
+        for i = selectedIndices
+            filename = csvFiles(i).name;
+            fileData = csvread(filename);
+            data{end+1, 1} = filename;
+            data{end, 2} = fileData;
         end
+
+        % Update the plot with the selected data
+        plotSelectedData(data);
     end
-    hold(plotAxis, 'off');
-    
-    xlabel(plotAxis, 'X Axis');
-    ylabel(plotAxis, 'Y Axis');
-    title(plotAxis, 'Multiple CSV Files Plot');
-    legend(plotAxis, 'Location', 'best');
+
+    % Callback function to plot selected data
+    function plotSelectedData(selectedData)
+        cla(plotAxis); % Clear the plot
+
+        hold(plotAxis, 'on');
+        for i = 1:length(selectedData)
+            filename = selectedData{i, 1};
+            fileData = selectedData{i, 2};
+
+            xData = fileData(:, 1);
+            yData = fileData(:, 2:end);
+
+            for j = 1:size(yData, 2)
+                plot(plotAxis, xData, yData(:, j), 'DisplayName', [filename, ' - Y', num2str(j)]);
+            end
+        end
+        hold(plotAxis, 'off');
+
+        xlabel(plotAxis, 'X Axis');
+        ylabel(plotAxis, 'Y Axis');
+        title(plotAxis, 'Multiple CSV Files Plot');
+        legend(plotAxis, 'Location', 'best');
+    end
 end
